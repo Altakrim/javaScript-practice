ad4a436a6e3b3aeea5989296afe9cd82669cae77
Altakrim <emd34057gmail.com>
2025-08-15 15:28:08 -1200

    typeof practice compleat

---
+++ Data-Type-02/type-01.js
@@ -0,0 +1,20 @@
+// Practice: ---> typeOf
+
+const price = 555; // This is a Number type
+console.log(typeof price);
+
+const learning = true; // This is a Boolean type
+console.log(typeof learning)
+
+const megaCity = "Dhaka";  // This is a String type
+console.log(typeof megaCity);
+
+const temperature = 102.5; // This is a Number type
+console.log(typeof temperature);
+
+const isHungry = true;
+console.log(typeof isHungry); // This is a Boolean type
+
+const isRice = true;  // This ia a Boolean type
+console.log(typeof isRice);
+
