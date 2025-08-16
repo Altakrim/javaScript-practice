f3841b2af859670651a341e9347af72d6e428864
Altakrim <emd34057gmail.com>
2025-08-15 15:05:15 -1200

    variables basic compleat

---
+++ Variables-Folder-01/Basic-Operator-05.js
@@ -0,0 +1,74 @@
+//Practice:-- > Basic Operator Declare
+
+// Problem - 1
+
+const income = 100;
+const cost = 40;
+const total = income - cost;
+console.log("The rest", total);
+
+// Problem -2
+
+const pencil = 10;
+const pencilCost = 2;
+const howUse = pencil / pencilCost;
+console.log("How Use:", howUse);
+
+// Problem - 3
+
+const myMoney = 60;
+const bayai = 15;
+const howCost = 60 / 15;
+console.log("How Cost:", howCost);
+
+// Problem -4
+
+const pastName = "Al takrim";
+const lastName = "Hossain Iptu";
+const fullName = pastName + lastName;
+console.log("Full Name:", fullName);
+
+// Problem - 5
+
+const weightRice = 153;
+const weightPulses = 261;
+const totalWeight = weightPulses + weightRice;
+console.log("Total Weight:", totalWeight); 
+
+// Problem - 6
+
+const isMoney = 500;
+const isCost = 75;
+const costHow = isMoney % isCost;
+console.log("How is store:", costHow);
+
+// Problem -7 
+const number = 8 ;
+const numberDivision = 3;
+const divisionResult = 8 / 3;
+const remainderResult = 8 % 3;
+console.log("Division Result:", divisionResult);
+console.log("Remainder Result:", remainderResult);
+
+// Problem - 8
+
+const amuon = 50;
+const remainderNumber = 9 ;
+const resultRemainder= 50 % 9;
+console.log(resultRemainder);
+
+// Problem - 9
+
+const bangla = "Bangla";
+const desh = "desh";
+const banglaDesh = bangla + desh;
+console.log("Bangla + desh:", banglaDesh);
+
+// Problem -10 
+
+const rice = 480;
+const costDay = 4;
+const totalCostDay = 480 / 4;
+console.log("Total Cost 3 Month and:", totalCostDay ,"Day");
+
+
---
+++ Variables-Folder-01/variable-declare-01.js
@@ -0,0 +1,11 @@
+// Practice :-- Variable Declare
+
+const birthday = 05-01-2002;
+const mySistrAndBorther = 3;
+const familyMember = 6 ;
+const javascript = 25;
+const rooms = 6;
+const study = 5 ;
+const total = 10;
+
+console.log(birthday, mySistrAndBorther, familyMember, javascript, rooms, study, total);
