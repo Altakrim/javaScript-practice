b58bad410e1ad23e66d7fb9c50b0e7d9c6cd647e
Altakrim <emd34057gmail.com>
2025-08-16 00:03:49 -1200

    comment practice

---
+++ Data-Type-02/let-02.js
@@ -0,0 +1,37 @@
+// Practice :-->  Let
+
+// Problem - 1
+let age = 20;
+age = 23;
+console.log("Age:", age);
+
+// Problem - 2
+let pocketMoney = 500;
+pocketMoney = 150;
+console.log("Pocket Money:", pocketMoney);
+
+// Problem - 3
+let studyTime = 4;
+studyTime = 3;
+console.log("Study Time:", studyTime);
+
+// Problem - 4
+let friendCount = 5;
+friendCount = 2;
+console.log("Friend Count:", friendCount);
+
+// Problem - 5
+let phonePrice = 25000;
+phonePrice = 12000;
+console.log("Phone Price:", phonePrice);
+
+// Problem - 6
+let chairsCount = 40;
+chairsCount = 38;
+chairsCount = 30;
+console.log("Chairs Count:", chairsCount);
+
+// Problem -7
+let playTime = 3;
+playTime = 2;
+console.log("Play Time:", playTime)
