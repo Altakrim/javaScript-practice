91349d99c51ffbcff8bea089ea3db8cbf110abff
Altakrim <emd34057gmail.com>
2025-08-20 21:13:14 -1200

    comparison Operator

---
+++ JavaScript-Books/Comparison-03/comparison-01.js
@@ -0,0 +1,32 @@
+/**
+ * 1. Greater than ( > )
+ * 2. Less than ( < )
+ * 3. Equal to ( == )
+ * 4. Greater than or Equal ( >= )
+ * 5. Less than or Equal ( <= )
+ * 6. Not Equal ( != )
+ * 7. AND ( && )
+ * 8. OR ( || )
+ * 
+ */
+
+// Problem - 1
+ console.log( 7 > 5 );
+
+ // Problem - 2
+ console.log( 45 < 50 );
+
+ // Problem - 3
+ console.log( 75 == 75);
+
+ // Problem - 4
+ console.log( 100 <= 95);
+
+ // Problem - 5
+ console.log( 10 != 7);
+
+ // Problem - 6
+ console.log( 20 <= 15);
+
+ // Problem - 7
+ console.log( 45 >= 95);
---
+++ JavaScript-Books/Comparison-03/comparison_operator-02.js
@@ -0,0 +1,50 @@
+// Comparison Operators:
+ /**
+  * 1. Greater than ( > )
+  * 2. Less than ( < )
+  * 3. Equal to ( == )
+  * 4. Not Equal ( != )
+  * 5. Greater than or Equal ( >= )
+  * 6. Less than or Equal ( <= )
+  */
+
+ // Problem - 1 
+ const myBrotherAge = 18;
+ const myAge = 24;
+    console.log("I am older:", myBrotherAge <= myAge);
+
+
+  // Problem -2 
+  const passMark = 33;
+  const studentMark = 45;
+  console.log("Student passed :", studentMark >= passMark);    
+
+  // Problem -3
+  const jamburaPrice = 100;
+  const kadebelePrice = 80;
+  console.log("Are the prices the same:", jamburaPrice == kadebelePrice);
+
+  // Problem -4
+  const iMmReadingABook = 10;
+  const myFriendReadingABook = 10;
+  console.log("Are we reading the same number of books:", iMmReadingABook == myFriendReadingABook);
+
+  // Problem - 5
+  const myBiscuitPis = 15;
+  const myFriendBiscuitPis = 20;
+  console.log("Whether the biscuits were divided equally:", myBiscuitPis == myFriendBiscuitPis); 
+
+  // Problem - 6
+  const realMadridGoals = 2;
+  const barcelonaGoals = 3;
+  console.log("Is Real Madrid scoring fewer goals than Barcelona:", realMadridGoals < barcelonaGoals);
+
+  // Problem - 7
+  const myName = "Rahim";
+  const myFriend = "Karim";
+  console.log("Do my friend and I have the same name:", myName == myFriend);
+
+  // Problem - 8
+  const iamWorking = true;
+  const myFriendIsWorking = false;
+  console.log("Are we both working:", iamWorking == myFriendIsWorking);
