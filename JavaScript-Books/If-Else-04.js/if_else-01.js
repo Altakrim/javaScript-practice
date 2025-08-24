7a06d4979234371e6574c59f18f6f3526b8711a6
Altakrim <emd34057gmail.com>
2025-08-23 12:46:13 -1200

    basic if/ else

---
+++ JavaScript-Books/If-Else-04.js/if_else-01.js
@@ -0,0 +1,50 @@
+// if else
+  
+    // Problem - 1
+    const timeEvening = 6;
+    if(timeEvening >= 6){
+        console.log("Will serve breakfast")
+    } else{
+        console.log("The broom will sweep the field.")
+    }
+
+    // Problem - 2
+    const mblAPP = "app";
+    if(mblAPP === "apps") {
+        console.log("Welcome !")
+    }else{
+        console.log("Get Lost")
+    }
+
+    // Problem - 3
+    const run = 5;
+    if(run > 4){
+        console.log("Give me chocolate")
+    }else{
+        console.log("No chocolate for you")
+    }
+
+    // Problem -4
+    const mark = 80;
+    if(mark >= 85){
+        console.log("father give me birk")
+    }else{
+        console.log("No birk")
+    }
+
+    // Problem - 5
+    const moveSnow = 9;
+    if(moveSnow > 8){
+        console.log("Move the snow")
+    }else{
+        console.log("No snow to move")
+    }
+
+    // Problem - 6
+    const temperature = 30;
+    if(temperature >= 30){
+         console.log("I sleep with a blanket")
+       
+    }else{
+          console.log("open the air conditioner")
+    }
